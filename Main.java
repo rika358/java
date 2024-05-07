4c23948a42d51cbc03383b1fc393d80947533907
rika358 <168232265+rika358@users.noreply.github.com>
2024-05-08 06:09:03 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args){
+    //処理
+  }
+}
+
+class Main {
+  public static void main(String[] args){
+    System.out.println("Hello World");
+    System.out.println(17);
+    System.out.println(20%3);
+
+    int number;
+    number = 3;
+    System.out.println(number);
+
+    int number1 = 5;
+    int number2 = 3;
+    System.out.println(number1+number2);
+    
+  }
+}
