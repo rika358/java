be04017b62eec5c3d71ed296c3e2b00123cecf8e
rika358 <168232265+rika358@users.noreply.github.com>
2024-05-08 06:31:05 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -23,6 +23,37 @@ class Main {
 
     text = "りょうま";
     System.out.println("私の名前は"+ text);
+
+    number = number + 5;
+    System.out.println(number);
+
+    number ++;
+    number += 1;
+    System.out.println(number);
+
+    int length = 5;
+    int height = 3;
+
+    int lectangleArea = lenght * heigt;
+    System.out.println(lectangleArea);
+
+    double number1 = 5.5;
+
+    int month = 12;
+    int date = 31;
+    
+    // 「12月31日」となるように変数と文字列を連結して出力してください
+    System.out.println(month + "月" + date + "日");
+    
+    // 7を2で割った値を出力してください
+    System.out.println(7/2);
+    
+    // 7.0を2.0で割った値を出力してください
+    System.out.println(7.0/2.0);
+    
+    // 7を2.0で割った値を出力してください
+    System.out.println(7/2.0);
+    
     
   }
 }
